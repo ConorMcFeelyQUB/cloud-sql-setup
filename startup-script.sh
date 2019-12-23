65ef74c2b152ddd8c7ecb619bcb5b204dc97861e
cmcfeely <cmcfeely03@qub.ac.uk>
2019-12-23 16:53:43 +0000

    will create vm to run sql code initialise and populate database

--- startup-script.sh
+++ startup-script.sh
@@ -1,5 +1,9 @@
 apt-get update
 apt-get install -yq git mysql-client
 
+git clone https://github.com/ConorMcFeelyQUB/cloud-sql-setup.git
+
+cd cloud-sql-setup/
+
 mysql --host=35.187.184.139 --user=root --password=QUBccProject -e \
 "source ./createandpop.sql"
