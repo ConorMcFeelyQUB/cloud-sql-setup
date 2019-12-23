109d3b552d1398aa1992e2649bf66432ebc2025c
ConorMcFeelyQUB <56089502+ConorMcFeelyQUB@users.noreply.github.com>
2019-12-23 21:31:17 +0000

    changes

--- startup-script.sh
+++ startup-script.sh
@@ -5,5 +5,15 @@ git clone https://github.com/ConorMcFeelyQUB/cloud-sql-setup.git
 
 cd cloud-sql-setup/
 
-mysql --host=35.187.184.139 --user=root --password=QUBccProject -e \
-"source ./createandpop.sql"
+#mysql --host=35.187.184.139 --user=root --password=QUBccProject -e \
+#"source ./createandpop.sql"
+
+GETIPADVERT="$(gcloud sql instances describe advert-db-instance --format='get(ipAddresses[0].ipAddress)')"
+
+GETIPPAGE="$(gcloud sql instances describe page-db-instance --format='get(ipAddresses[0].ipAddress)')"
+
+mysql --host="${GETIPPAGE}" --user=root --password=QUBccProject -e \
+"source ./createandpoppage.sql" \
+
+mysql --host="${GETIPADVERT}" --user=root --password=QUBccProject -e \
+"source ./createandpopadvert.sql" \
